947a06900a7e9cf8f07bb482ba787f316eba2361
Tom White <thomas.white@sony.com>
2022-03-27 22:55:21 +0100

    css changes

--- src/components/DataGraph/index.js
+++ src/components/DataGraph/index.js
@@ -4,18 +4,23 @@ import React from "react";
 // import npm packages
 // area chart component and functions
 import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
+// functions to manage datetime picker ranges
+import { format } from 'date-fns';
 
 // import self defined css
 import './DataGraph.css';
 
 // react comonent to manage tableData and search functionality
 const ReactDataGraph = ({ title='', graphData=[], titleStyle={}, removedHeadings=[], headingTextOverride=[] /* {key, text} */, tableDataOveride=[] /* {key, function} */, rowsShown=20, idSortedby, sortType }) => {
+  graphData.forEach((ele) => {
+    ele.date = format(new Date(ele?.date), "yyyy-MM-dd HH:mm")
+  })
   return (
     <>
       {title ? <h2>{title}</h2> : <></>}
       <div className="dataGraphContainer" >
           <ResponsiveContainer>
-              <AreaChart data={graphData} margin={{top: 50,right: 30,left: 50,bottom: 50,}}>
+              <AreaChart data={graphData} style={{ fill: 'rgb(255, 245, 240)' }} margin={{top: 50,right: 30,left: 50,bottom: 50,}}>
                   <defs>
                       <linearGradient id="priceUsd" x1="0" y1="0" x2="0" y2="1">
                       <stop offset="5%" stopColor="#968635" stopOpacity={0.8}/>
